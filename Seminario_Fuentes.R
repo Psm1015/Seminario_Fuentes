66abe02364765ade0dc00dbc14a68906c03f068a
Tu Nombre <tu.email@ejemplo.com>
2024-10-21 10:44:16 +0200

    Explicación error

--- Seminario_Fuentes.R
+++ Seminario_Fuentes.R
@@ -6,7 +6,7 @@ library(pxR)
 library(jsonlite)
 
 Alimentacion <- fromJSON("INPUT/DATA/Alimentacion.json")#hemos usado IA para solucionar un error. 
-#hemos tenido que instalar el paquete jsonlite, ya que con rjson nos salia este erro: 
+#hemos tenido que instalar el paquete jsonlite, ya que con rjson nos salia este error: 
 #Error: unable to find an inherited method for function ‘fromJSON’ for signature ‘content = "missing", handler = "missing"’
 #Al utilizar este paquete se utiliza el fromjson de este paquete y hay que quitar el file= del paquete rjson
 Indice_masa_corporal <- fromJSON("INPUT/DATA/Indice-masa-corporal.json")
