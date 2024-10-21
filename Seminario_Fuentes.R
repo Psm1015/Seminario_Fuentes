2a5d622d5c7d96dd53308852bdfef3afdb2431c7
Diego González Sánchez <dgs1016@alu.ubu.es>
2024-10-21 10:10:57 +0200

    Cargando paquete pcAxis

--- Seminario_Fuentes.R
+++ Seminario_Fuentes.R
@@ -1,6 +1,8 @@
 #Script para el seminario de fuentes
 library(tidyverse)
 library(rjson)
+library(pxR)
 
 Alimentacion <- fromJSON(file = "INPUT/DATA/Alimentacion.json")
 Indice_masa_corporal <- fromJSON(file = "INPUT/DATA/Indice-masa-corporal.json")
+Desempleo <- read.px("INPUT/DATA/Desempleo.px")
