53e61f169cabd700710a1a4a782d3b96ac29f562
Tu Nombre <tu.email@ejemplo.com>
2024-10-21 12:00:16 +0200

    Carga de datos en Desempleo

---
+++ Desempleo.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(tidyverse)
+library(pxR)
+
+Desempleo <- read.px("INPUT/DATA/Desempleo.px")
+# Ver la estructura del archivo cargado
+str(Desempleo)
+
+# Convertir a un data frame para trabajar con los datos
+df <- as.data.frame(Desempleo)
+df
+view(df)
