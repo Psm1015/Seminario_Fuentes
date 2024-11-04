40e9876b21825c49bbda246e8b9c1f34fe406b81
unknown <psm1015@alu.ubu.es>
2024-11-04 10:32:30 +0100

    IMC

--- IMC.R
+++ IMC.R
@@ -7,5 +7,12 @@ library(rjson)
 library(jsonlite)
 
 IMC<- fromJSON("INPUT/DATA/Indice-masa-corporal.json")
-spread_all(IMC)
+spread_all(IMC, json.column = "MetaData")
 view(IMC)
+IMC %>% 
+     spread_all() %>% 
+     View()
+IMC %>% 
+  gather_object %>% 
+  json_types %>% 
+  count(name, type)
