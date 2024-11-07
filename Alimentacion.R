8b7f765fd06a6af39bbb0350f78c9bd7cd1dbd33
Diego González Sánchez <dgs1016@alu.ubu.es>
2024-11-07 14:04:42 +0100

    Alimentacion

--- Alimentacion.R
+++ Alimentacion.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyverse)
 library(tidyr)
+library(purrr)
 library(tidyjson)
 library(rjson)
 library(RJSONIO)
@@ -9,7 +10,55 @@ library(RJSONIO)
 Alimentacion <- fromJSON("INPUT/DATA/Alimentacion.json") 
 str(Alimentacion)
 view(Alimentacion)
+head(Alimentacion)
 
+Alimentacion %>% 
+  spread_all() %>% 
+  View()
+
+Alimentacion %>% 
+  spread_all() %>% 
+  gather_object %>% 
+  json_types %>% 
+  count(name, type)
+
+resultado <- Alimentacion %>%
+  enter_object("Data") %>%
+  gather_array() %>%
+  unnest_wider(json.column = ..JSON) %>%  # Especificamos la columna correcta
+  enter_object("MetaData") %>%
+  gather_array() %>%
+  unnest_wider(json.column = ..JSON) %>%  # Igual para MetaData
+  select(-document.id, -array.index)  # Eliminar columnas innecesarias
+
+# Inspeccionar el resultado
+glimpse(resultado)
+
+
+# Inspeccionar la estructura del resultado
+glimpse(resultado)
+
+# Inspeccionar la estructura del resultado
+glimpse(resultado)
+
+# Inspeccionar la estructura de los datos antes de usar View()
+glimpse(resultado)  # Muestra una vista compacta de las primeras filas
+head(resultado)  # Muestra las primeras filas de los datos
+
+Alimentacion_na %>% 
+  spread_all() %>% 
+  gather_object %>% 
+  json_types %>% 
+  count(name, type)
+
+Alimentacion %>% enter_object(MetaData) %>% glimpse()
+
+Alimentacion %>%
+  enter_object(MetaData) %>%
+  unnest_wider(..JSON, names_sep = "_") %>%  # Generar nombres automáticos con el separador "_"
+  select(-document.id) %>%  # Eliminar document.id si no lo necesitas
+  view()  # Mostrar los resultados
+  
 # Cambiar los rangos de edad
 Alimentacion_full <- Alimentacion %>%
   mutate(MetaData = case_when(
