0a472f4186886a1b6f810032da30010cb09e2b40
jhq1002 <jhq1002@alu.ubu.es>
2024-11-08 01:15:05 +0100

    Actualización desempleo

--- Desempleo.R
+++ Desempleo.R
@@ -36,17 +36,19 @@ view(df11)
 
 Desempleo_full <- df11 %>%
   mutate(Edad = case_when(
-    Edad %in% c('De 16 a 19 aÃ±os', 'De 20 a 24 aÃ±os') ~ '18 a 24',
-    Edad %in% c('De 25 a 29 aÃ±os', 'De 30 a 34 aÃ±os', 'De 35 a 39 aÃ±os', 'De 40 a 44 aÃ±os', 'De 45 a 49 aÃ±os', 'De 50 a 54 aÃ±os', 'De 55 a 59 aÃ±os', 'De 60 a 64 aÃ±os') ~ '25 a 64',
-    Edad %in% c('De 65 a 69 aÃ±os', '70 y mÃ¡s aÃ±os') ~ '65 o más',
-    TRUE ~ Edad # Mantener el valor actual si no coincide con ningún rango
+    df11$Edad == c('De 16 a 19 aÃ±os', 'De 20 a 24 aÃ±os') ~ '18 a 24',
+    df11$Edad == c('De 25 a 29 aÃ±os', 'De 30 a 34 aÃ±os', 'De 35 a 39 aÃ±os', 'De 40 a 44 aÃ±os', 'De 45 a 49 aÃ±os', 'De 50 a 54 aÃ±os', 'De 55 a 59 aÃ±os', 'De 60 a 64 aÃ±os') ~ '25 a 64',
+    df11$Edad == c('De 65 a 69 aÃ±os', '70 y mÃ¡s aÃ±os') ~ '65 o más',
   ))
 
-str(Desempleo_full)
-unique(df11$Edad)
-#df11$Edad <- iconv(df11$Edad, from = "ISO-8859-1", to = "UTF-8")
-df11$Edad
 
+Desempleo_full
+view(Desempleo_full)
+
+#str(Desempleo_full)
+#unique(df11$Edad)
+#df11$Edad <- iconv(df11$Edad, from = "ISO-8859-1", to = "UTF-8")
+#df11$Edad
 
 
 
