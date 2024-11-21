0f7989cf85fbaa7172334c9f66729a20ddcd4c83
jhq1002 <jhq1002@alu.ubu.es>
2024-11-21 12:09:13 +0100

    Actualización desempleo

--- Desempleo.R
+++ Desempleo.R
@@ -21,34 +21,61 @@ view(df11)
 
 #Rango de edades: 18 a 24, 25 a 64, 65 o más
 #Agrpuamineto de las edades por los rangos que hemos deifinifo
-Desempleo_full <- df11 %>%
-  mutate(Grupo_edad = case_when(
-    Edad == "De 16 a 19 aÃ±os" ~ "18 a 24 aÃ±os",
-    Edad == "De 20 a 24 aÃ±os" ~ "18 a 24 aÃ±os",
-    Edad == "De 25 a 29 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 30 a 34 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 35 a 39 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 40 a 44 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 45 a 49 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 50 a 54 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 55 a 59 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 60 a 64 aÃ±os" ~ "25 a 64 aÃ±os",
-    Edad == "De 65 a 69 aÃ±os" ~ "65 o mÃ¡s",
-    Edad =="70 y mÃ¡s aÃ±os" ~ '65 o mÃ¡s',
-  ))
+#Desempleo_full <- df11 %>%
+ # mutate(Grupo_edad = case_when(
+  #  Edad == "De 16 a 19 aÃ±os" ~ "18 a 24 aÃ±os",
+   # Edad == "De 20 a 24 aÃ±os" ~ "18 a 24 aÃ±os",
+    #Edad == "De 25 a 29 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 30 a 34 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 35 a 39 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 40 a 44 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 45 a 49 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 50 a 54 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 55 a 59 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 60 a 64 aÃ±os" ~ "25 a 64 aÃ±os",
+    #Edad == "De 65 a 69 aÃ±os" ~ "65 o mÃ¡s",
+    #Edad =="70 y mÃ¡s aÃ±os" ~ '65 o mÃ¡s',
+ # ))
+#view(Desempleo_full)
+#colnames(Desempleo_full)
+
+Desempleo_full1 <- df11 %>%
+  transmute(
+    Edad = case_when(
+      Edad == "De 16 a 19 aÃ±os" ~ "De 18 a 24 años",
+      Edad == "De 20 a 24 aÃ±os" ~ "De 18 a 24 años",
+      #    Edad == "De 25 a 34 aÃ±os" ~ "De 25 a 64 aÃ±os",
+      Edad == "De 25 a 29 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 30 a 34 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 35 a 39 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 40 a 44 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 45 a 49 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 50 a 54 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 55 a 59 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 60 a 64 aÃ±os" ~ "De 25 a 64 años",
+      Edad == "De 65 a 69 aÃ±os" ~ "65 o más",
+      Edad == "70 y mÃ¡s aÃ±os" ~ "65 o más",
+    ),
+    Periodo = Periodo,
+    Tiempo.de.bÃºsqueda.de.empleo=Tiempo.de.bÃºsqueda.de.empleo,
+    Edad = Edad,
+    Sexo = Sexo,
+    value = value,
+  )
+view(Desempleo_full1)
 
 
 #Eliminar filas es la que salia el valor NA
-Desempleo_full1 <- Desempleo_full[-c((1:144), (1873:2016), (3745:3888)), ]
-Desempleo_full1
-view(Desempleo_full1)
+Desempleo_full11 <- Desempleo_full1[-c((1:144), (1873:2016), (3745:3888)), ]
+Desempleo_full11
+view(Desempleo_full11)
 
-Desempleo_df <- Desempleo_full1 %>% select(-Edad)
-view(Desempleo_df)
+#Desempleo_df <- Desempleo_full11 %>% select(-Edad)
+#view(Desempleo_df)
 
 
 #Para eliminar los valores de ambos sexos y total
-datos_Desempleo <-  Desempleo_df %>%
+datos_Desempleo <-  Desempleo_full11 %>%
   filter(
     Sexo != "Ambos sexos",                                  
     Tiempo.de.bÃºsqueda.de.empleo != "Total",
@@ -61,7 +88,7 @@ view(datos_Desempleo)
 library(ggplot2)
 
 # Gráfica de desempleo por grupo de edad en 2021
-ggplot(datos_Desempleo, aes(x = Grupo_edad, y = value, fill = Sexo)) +
+ggplot(datos_Desempleo, aes(x = Edad, y = value, fill = Sexo)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(title = "Desempleo por Grupo de Edad en 2021", x = "Grupo de Edad", y = "Cantidad de Desempleo") +
   scale_fill_manual(values = c("Hombres" = "steelblue", "Mujeres" = "salmon")) +
@@ -72,18 +99,19 @@ ggplot(datos_Desempleo, aes(x = Grupo_edad, y = value, fill = Sexo)) +
 
 
 #Gráfica para observar la tendencia de desempleo en distintos periodos
-ggplot(datos_Desempleo, aes(x = Periodo, y = value, fill = Grupo_edad)) +
+ggplot(datos_Desempleo, aes(x = Periodo, y = value, fill = Edad)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Tendencia del Desempleo por Grupo de Edad", x = "Año", y = "Cantidad de Desempleo") +
-  scale_fill_manual(values = c("18 a 24 aÃ±os" = "steelblue", "25 a 64 aÃ±os" = "salmon", "65 o mÃ¡s" = "yellow")) +
+  labs(title = "Tendencia del Desempleo por Edad", x = "Año", y = "Cantidad de Desempleo") +
+  scale_fill_manual(values = c("De 18 a 24 años" = "steelblue", "De 25 a 64 años" = "salmon", "65 o más" = "yellow")) +
   theme_minimal() +
   theme(
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
 
 
+
 #Gráfica para comparar el desempleo entre hombres y mujeres en 2001 confiltro Grupo_edad
-ggplot(datos_Desempleo %>% filter(Grupo_edad == "18 a 24 aÃ±os"), aes(x = Sexo, y = value, fill = Sexo)) +
+ggplot(datos_Desempleo %>% filter(Edad == "De 18 a 24 años"), aes(x = Sexo, y = value, fill = Sexo)) +
   geom_bar(stat = "identity") +
   labs(title = "Comparación de Desempleo entre Hombres y Mujeres (18 a 24 años, 2001)", x = "Sexo", y = "Cantidad de Desempleo") +
   scale_fill_manual(values = c("Hombres" = "steelblue", "Mujeres" = "salmon")) +
@@ -93,6 +121,14 @@ ggplot(datos_Desempleo %>% filter(Grupo_edad == "18 a 24 aÃ±os"), aes(x = Sexo
   )
 
 
+Desempleo_IMC <- inner_join(
+  datos_Desempleo, 
+  datos_IMC_df, 
+  by = join_by(Edad, Sexo),
+  relationship = "many-to-many" #generará un nuevo data frame con todas las combinaciones de 
+                                #filas coincidentes entre los dos marcos de datos
+)
+view(Desempleo_IMC)
 
 
 
