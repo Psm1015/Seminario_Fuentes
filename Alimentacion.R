3cd9258b76bee7cc0dd2dc8cef828e58333659e7
Diego González Sánchez <dgs1016@alu.ubu.es>
2024-11-21 11:45:04 +0100

    Cambio en el join entre IMC y Alimentacion

--- Alimentacion.R
+++ Alimentacion.R
@@ -50,34 +50,54 @@ view(DF1)
 #  ))
 #datos_df
 #view(datos_df)
+# Cambiar los rangos de edad en la columna Edad.
 datos_df1 <- DF1 %>%
   transmute(
     Edad = case_when(
-      Edad == "De 15 a 24 aÃ±os" ~ "De 18 a 24 aÃ±os",
+      Edad == "De 15 a 24 aÃ±os" ~ "De 18 a 24 años",
       #    Edad == "De 25 a 34 aÃ±os" ~ "De 25 a 64 aÃ±os",
-      Edad == "De 35 a 44 aÃ±os" ~ "De 25 a 64 aÃ±os",
+      Edad == "De 35 a 44 aÃ±os" ~ "De 25 a 64 años",
       #    Edad == "De 45 a 54 aÃ±os" ~ "De 25 a 64 aÃ±os",
       #   Edad == "De 55 a 64 aÃ±os" ~ "De 25 a 64 aÃ±os",
-      Edad == "De 65 a 74 aÃ±os" ~ "De 65 y mÃ¡s aÃ±os",
+      Edad == "De 65 a 74 aÃ±os" ~ "De 65 y más años",
       #    Edad == "De 75 y mÃ¡s aÃ±os" ~ "De 65 y mÃ¡s aÃ±os"
     ),
-    Frecuencia = Frecuencia,
-    Alimentos = Alimentos,
+    Frecuencia = case_when(
+      Frecuencia == "3 o mÃ¡s veces a la semana pero no a diario" ~ "3 o más veces a la semana pero no a diario",
+      Frecuencia == "Menos de 1 vez a la semana" ~ "Menos de 1 vez a la semana",
+      Frecuencia == "1 o 2 veces a la semana" ~ "1 o 2 veces a la semana",
+      Frecuencia == "A diario" ~ "A diario",
+      Frecuencia == "TOTAL" ~ "TOTAL",
+      Frecuencia == "Nunca" ~ "Nunca"
+    ),
+    Alimentos = case_when(
+      Alimentos == "Fruta fresca (excluye zumos)" ~ "Fruta fresca (excluye zumos)",
+      Alimentos == "Carne" ~ "Carne",
+      Alimentos == "Huevos" ~ "Huevos",
+      Alimentos == "Pescado" ~ "Pescado",
+      Alimentos == "Pasta,arroz,patatas" ~ "Pasta,arroz,patatas",
+      Alimentos == "Pan,cereales" ~ "Pan,cereales",
+      Alimentos == "Verduras, ensaladas y hortalizas" ~ "Verduras, ensaladas y hortalizas",
+      Alimentos == "Legumbres" ~ "Legumbres",
+      Alimentos == "Embutidos y fiambres" ~ "Embutidos y fiambres",
+      Alimentos == "Productos lÃ¡cteos" ~ "Productos lácteos",
+      Alimentos == "Dulces" ~ "Dulces",
+      Alimentos == "Refrescos con azÃºcar" ~ "Refrescos con azúcar",
+      Alimentos == "Comida rÃ¡pida" ~ "Comida rápida",
+      Alimentos == "Aperitivos o comidas saladas de picar" ~ "Aperitivos o comidas saladas de picar",
+      Alimentos == "Zumo natural de frutas o verduras" ~ "Zumo natural de frutas o verduras"
+    ),
     Sexo = Sexo,
     value = value
   )
 view(datos_df1)
 
-# ELIMINAR TODAS LAS FILAS QUE TENGAN NA EN LA COLUMNA Grupo_edad
+# ELIMINAR TODAS LAS FILAS QUE TENGAN NA EN LA COLUMNA Edad
 df_sin_NA <- datos_df1 %>% 
   filter(!is.na(Edad))
 df_sin_NA
 view(df_sin_NA)
 
-#ELIMINAR LA COLUMNA EDAD
-Alimentacion_df <- df_sin_NA %>% 
-  select(-Edad)
-view(Alimentacion_df)
 
 datos_Alimentacion <-  df_sin_NA%>%
   filter(
@@ -114,7 +134,7 @@ ggplot(Alimentacion_ADiario, aes(x = Edad, y = value, fill = Alimentos)) +
 # 3 O MÁS VECES A LA SEMANA PERO NO A DIARIO
 Alimentacion_pescado <- datos_Alimentacion %>%
   filter(
-    Frecuencia == "3 o mÃ¡s veces a la semana pero no a diario",
+    Frecuencia == "3 o más veces a la semana pero no a diario",
     Alimentos == "Pescado"
   )
 view (Alimentacion_pescado)
@@ -136,7 +156,7 @@ ggplot(Alimentacion_pescado, aes(x = Edad, y = value, fill = Sexo)) +
 # 3 O MÁS VECES A LA SEMANA PERO NO A DIARIO
 Alimentacion_carne <- datos_Alimentacion %>%
   filter(
-    Frecuencia == "3 o mÃ¡s veces a la semana pero no a diario",
+    Frecuencia == "3 o más veces a la semana pero no a diario",
     Alimentos == "Carne"
   )
 view(Alimentacion_carne)
@@ -158,7 +178,7 @@ ggplot(Alimentacion_carne, aes(x = Edad, y = value, fill = Sexo)) +
 Alimentacion_rapida <- datos_Alimentacion %>%
   filter(
     Frecuencia == "1 o 2 veces a la semana",
-    Alimentos == "Comida rÃ¡pida"
+    Alimentos == "Comida rápida"
   )
 view(Alimentacion_rapida)
 ggplot(Alimentacion_rapida, aes(x = Edad, y = value, fill = Sexo)) +
@@ -176,8 +196,20 @@ ggplot(Alimentacion_rapida, aes(x = Edad, y = value, fill = Sexo)) +
 
 Alimentacion_IMC <- inner_join(
   Alimentacion_rapida,  # Primer DataFrame
-  df_tabla_media,
+  datos_IMC_df,
   by = join_by(Edad, Sexo)# Segundo DataFrame  ,  # Tipo de join: "inner"  # Para diferenciar las columnas con el mismo nombre
 )
-view(df_combined)
+view(Alimentacion_IMC)
+
+
+# Solo queremos ver las filas donde las personas sean obesas (Obesidad (IMC>=30 kg/m2))
+C.Rapida_Obesidad <- Alimentacion_IMC %>%
+  filter(
+    Masa.corporal.adultos == "Obesidad (IMC>=30 kg/m2)",    # Solo obesidad
+    Sexo != "Ambos sexos",                                  # Excluir ambos sexos
+    Edad != "TOTAL",                                        # Excluir edad total
+    Nivel.de.estudios != "TOTAL"                            # Excluir nivel de estudios total
+  )
+View(C.Rapida_Obesidad)
+
 
